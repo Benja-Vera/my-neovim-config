7069f1133795dc9be7ad22300de98861975eb163
Benja Vera <benjamin.valdes@ug.uchile.cl>
2025-07-07 16:13:08 -0400

    Disabled just the tresitter highlighting for latex

--- init.lua
+++ init.lua
@@ -976,10 +976,10 @@ require('lazy').setup({
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'yaml', 'python' },
       -- Autoinstall languages that are not installed
-      ignore_install = { 'latex' },
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'latex' },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
