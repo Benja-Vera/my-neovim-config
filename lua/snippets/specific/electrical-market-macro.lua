43e19ee443a36eda07b368a9a8a12ed593a9d959
Benja Vera <benjamin.valdes@ug.uchile.cl>
2026-01-06 13:40:33 -0300

    Added parameter postfixes to EM snippets

--- lua/snippets/specific/electrical-market-macro.lua
+++ lua/snippets/specific/electrical-market-macro.lua
@@ -4,13 +4,16 @@ local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 
-local postfix = require('luasnip.extras.postfix').postfix
-
 local in_mathzone = function()
   return vim.fn['vimtex#syntax#in_mathzone']() == 1
 end
 
 return {
+  -- tt -> \tax
+  s({ trig = 'tt', snippetType = 'autosnippet', condition = in_mathzone }, {
+    t '\\tax',
+  }),
+
   -- aa -> \bid
   s({ trig = 'aa', snippetType = 'autosnippet', condition = in_mathzone }, {
     t '\\bid',
@@ -26,24 +29,9 @@ return {
     t '\\production',
   }),
 
-  -- ff -> \flow
-  s({ trig = 'ff', snippetType = 'autosnippet', condition = in_mathzone }, {
-    t '\\flow',
-  }),
-
-  -- dd -> \demand
-  s({ trig = 'dd', snippetType = 'autosnippet', condition = in_mathzone }, {
-    t '\\demand',
-  }),
-
-  -- rr -> \resistance
-  s({ trig = 'rr', snippetType = 'autosnippet', condition = in_mathzone }, {
-    t '\\resistance',
-  }),
-
-  -- sgn -> \sgn(\node, \edge)
-  s({ trig = 'sgn', snippetType = 'autosnippet', condition = in_mathzone }, {
-    t '\\sgn(\\node, \\edge)',
+  -- cc -> \isocost
+  s({ trig = 'cc', snippetType = 'autosnippet', condition = in_mathzone }, {
+    t '\\isocost',
   }),
 
   -- mm -> \kktcapacitylower
@@ -51,9 +39,14 @@ return {
     t '\\kktcapacitylower',
   }),
 
-  -- ll -> \pollutioncost
-  s({ trig = 'll', snippetType = 'autosnippet', condition = in_mathzone }, {
-    t '\\pollutioncost',
+  -- mM -> \kktcapacityupper
+  s({ trig = 'mM', snippetType = 'autosnippet', condition = in_mathzone }, {
+    t '\\kktcapacityupper',
+  }),
+
+  -- dd -> \demand
+  s({ trig = 'dd', snippetType = 'autosnippet', condition = in_mathzone }, {
+    t '\\demand',
   }),
 
   -- pp -> \price
@@ -61,9 +54,29 @@ return {
     t '\\price',
   }),
 
-  -- xx -> x_{n, t}
+  -- rr -> \resistance
+  s({ trig = 'rr', snippetType = 'autosnippet', condition = in_mathzone }, {
+    t '\\resistance',
+  }),
+
+  -- ff -> \flow
+  s({ trig = 'ff', snippetType = 'autosnippet', condition = in_mathzone }, {
+    t '\\flow',
+  }),
+
+  -- sgn -> \sgn(\node, \edge)
+  s({ trig = 'sgn', snippetType = 'autosnippet', condition = in_mathzone }, {
+    t '\\sgn(\\node, \\edge)',
+  }),
+
+  -- ll -> \pollutioncost
+  s({ trig = 'll', snippetType = 'autosnippet', condition = in_mathzone }, {
+    t '\\pollutioncost',
+  }),
+
+  -- xx -> x_{\node, \tech}
   s({ trig = 'xx', snippetType = 'autosnippet', condition = in_mathzone }, {
-    t 'x_{n, t}',
+    t '\\deviation',
   }),
 
   -- sumnt -> \sum_{\node, \tech}
@@ -91,19 +104,65 @@ return {
     t '\\text{NT}^\\text{CL}(\\boldsymbol{q})',
   }),
 
-  postfix({
-    trig = '.p',
-    match_pattern = '\\(bid|production)$',
+  -- \bid.p -> \bid[\node_0][\tech_0] (postfix)
+  s({
+    trig = '(\\\\bid|\\\\production|\\\\pollutionfactor|\\\\kktcapacitylower|\\\\kktcapacityupper|\\\\isocost|\\\\deviation).p',
+    regTrig = true,
+    trigEngine = 'ecma',
     snippetType = 'autosnippet',
     condition = in_mathzone,
+    desc = 'add parameters',
   }, {
-    f(function(_, parent)
-      return parent.snippet.env.POSTFIX_MATCH
+    f(function(_, snip)
+      return snip.captures[1] .. '['
     end),
-    t '[',
-    i(1, 'n'),
+    i(1, '\\node_0'),
     t '][',
-    i(2, 't'),
+    i(2, '\\tech_0'),
+    t '] ',
+  }),
+
+  -- \demand.p -> \demand[\node_0] (postfix)
+  s({
+    trig = '(\\\\demand|\\\\price).p',
+    regTrig = true,
+    trigEngine = 'ecma',
+    snippetType = 'autosnippet',
+    condition = in_mathzone,
+    desc = 'add parameters',
+  }, {
+    f(function(_, snip)
+      return snip.captures[1] .. '['
+    end),
+    i(1, '\\node_0'),
+    t ']',
+  }),
+
+  -- \resistance.p -> \resistance[\edge_0] (postfix)
+  s({
+    trig = '(\\\\resistance|\\\\flow).p',
+    regTrig = true,
+    trigEngine = 'ecma',
+    snippetType = 'autosnippet',
+    condition = in_mathzone,
+    desc = 'add parameters',
+  }, {
+    f(function(_, snip)
+      return snip.captures[1] .. '['
+    end),
+    i(1, '\\edge_0'),
+    t ']',
+  }),
+
+  -- \pollutioncost.p -> \pollutioncost[\tech_0] (postfix)
+  s({
+    trig = '\\pollutioncost.p',
+    snippetType = 'autosnippet',
+    condition = in_mathzone,
+    desc = 'add parameters',
+  }, {
+    t '\\pollutioncost[',
+    i(1, '\\tech_0'),
     t ']',
   }),
 }
