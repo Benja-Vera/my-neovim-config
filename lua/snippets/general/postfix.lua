bbba5f8a95a15e4721c4e50fc3c229dc4f0d6b61
Benja Vera <benjamin.valdes@ug.uchile.cl>
2026-02-09 15:21:12 -0300

    Improved bar snippet to add {} and applied show condition

--- lua/snippets/general/postfix.lua
+++ lua/snippets/general/postfix.lua
@@ -10,44 +10,85 @@ local in_mathzone = function()
   return vim.fn['vimtex#syntax#in_mathzone']() == 1
 end
 
+-- Don't show snippet
+local no_show = function()
+  return false
+end
+
 return {
   -- \alphabar -> \overline{\alpha}
-  postfix({ trig = 'bar', match_pattern = [[[\\%w%.%_%-%"%']+$]], condition = in_mathzone, snippetType = 'autosnippet', dscr = 'postfix bar' }, {
+  -- \mathbb{R}bar -> \overline{\mathbb{R}} (also allowed)
+  postfix({
+    trig = 'bar',
+    match_pattern = [[[\\%w%.%_%-%"%'%{%}]+$]],
+    condition = in_mathzone,
+    show_condition = no_show,
+    snippetType = 'autosnippet',
+    dscr = 'postfix bar',
+  }, {
     f(function(_, parent)
       return '\\overline{' .. parent.snippet.env.POSTFIX_MATCH .. '}'
     end, {}),
   }),
 
   -- \alphabf -> \boldsymbol{\alpha}
-  postfix({ trig = 'bf', match_pattern = [[[\\%w%.%_%-%"%']+$]], condition = in_mathzone, snippetType = 'autosnippet', dscr = 'postfix bold' }, {
+  postfix({
+    trig = 'bf',
+    match_pattern = [[[\\%w%.%_%-%"%']+$]],
+    condition = in_mathzone,
+    show_condition = no_show,
+    snippetType = 'autosnippet',
+    dscr = 'postfix bold',
+  }, {
     f(function(_, parent)
       return '\\boldsymbol{' .. parent.snippet.env.POSTFIX_MATCH .. '}'
     end, {}),
   }),
 
   -- \alphahat -> \hat{\alpha}
-  postfix({ trig = 'hat', match_pattern = [[[\\%w%.%_%-%"%']+$]], condition = in_mathzone, snippetType = 'autosnippet', dscr = 'postfix hat' }, {
+  postfix({
+    trig = 'hat',
+    match_pattern = [[[\\%w%.%_%-%"%']+$]],
+    condition = in_mathzone,
+    show_condition = no_show,
+    snippetType = 'autosnippet',
+    dscr = 'postfix hat',
+  }, {
     f(function(_, parent)
       return '\\hat{' .. parent.snippet.env.POSTFIX_MATCH .. '}'
     end, {}),
   }),
 
   -- \alphavec -> \vec{\alpha}
-  postfix({ trig = 'vec', match_pattern = [[[\\%w%.%_%-%"%']+$]], condition = in_mathzone, snippetType = 'autosnippet', dscr = 'postfix vector' }, {
+  postfix({
+    trig = 'vec',
+    match_pattern = [[[\\%w%.%_%-%"%']+$]],
+    condition = in_mathzone,
+    show_condition = no_show,
+    snippetType = 'autosnippet',
+    dscr = 'postfix vector',
+  }, {
     f(function(_, parent)
       return '\\vec{' .. parent.snippet.env.POSTFIX_MATCH .. '}'
     end, {}),
   }),
 
   -- \alphatil -> \tilde{\alpha}
-  postfix({ trig = 'til', match_pattern = [[[\\%w%.%_%-%"%']+$]], condition = in_mathzone, snippetType = 'autosnippet', dscr = 'postfix tilde' }, {
+  postfix({
+    trig = 'til',
+    match_pattern = [[[\\%w%.%_%-%"%']+$]],
+    condition = in_mathzone,
+    show_condition = no_show,
+    snippetType = 'autosnippet',
+    dscr = 'postfix tilde',
+  }, {
     f(function(_, parent)
       return '\\tilde{' .. parent.snippet.env.POSTFIX_MATCH .. '}'
     end, {}),
   }),
 
-  -- Underbrace: (something)./ -> \underbrace{something}_{|}
-  postfix({ trig = '._', match_pattern = '%b()', snippetType = 'autosnippet', condition = in_mathzone }, {
+  -- Underbrace: (something)._ -> \underbrace{something}_{|}
+  postfix({ trig = '._', match_pattern = '%b()', snippetType = 'autosnippet', condition = in_mathzone, show_condition = no_show }, {
     f(function(_, parent)
       return '\\underbrace{' .. parent.snippet.env.POSTFIX_MATCH:sub(2, -2) .. '}_{'
     end, {}),
